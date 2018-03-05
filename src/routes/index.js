8e73a859c4351a0711c263326732fb30cfd0fdfb
muralimgwl <murali.m@goodworklabs.com>
2018-03-05 18:10:19 +0530

    Added inner object configuration

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import Landing from '../components/content/Landing';
-import Dashboard from '../components/content/Dashboard';
-import Demo from '../components/content/Demo'
-import ChessDemo from '../components/content/ChessDemo'
+
 
 
 
@@ -14,7 +12,6 @@ const Main = () => {
       <main style={{"marginBottom": "50px"}}>
         <Switch>
           <Route exact path={base+"/"} component={Landing} />
-          <Route exact path={base+"/dashboard/:component?"} component={Dashboard} />
         </Switch>
       </main>
      )
